25a7dfc9432b4c61b2a1ba29909aba3498f4e13d
EkaterinaI <white-angel666@mail.ru>
2018-05-10 21:36:28 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import Weather from "./components/Weather";
 const API_KEY = "502f4bef0c6a6c724bc1612c6cfe292e";
 
 var citiesMap = [
-  'london',
-  'kiev',
-  'manchester',
-  'odessa',
-  'moscow'
+  'London',
+  'Kiev',
+  'Manchester',
+  'Odessa',
+  'Moscow'
+
 ]
+if(localStorage.length) {
+
+} else {
+  localStorage.setItem('citiesMap', JSON.stringify(citiesMap));
+}
+console.log(citiesMap)
 
 class App extends React.Component {
   state = {
@@ -28,30 +35,38 @@ class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
+    city.toLowerCase();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    if (city) {
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: "",
-        textvalue: 'C' 
-
-      });
-    } else {
-      this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "Please enter the values.",
-        cityName: ''
-      });
+    try {
+      const data = await api_call.json();
+      if (city) {
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: "",
+          textvalue: 'C' 
+  
+        });
+      } else {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Please enter the values.",
+          cityName: ''
+        });
+      }
     }
+    catch(error) {
+      console.log(error);
+      alert("vvedite city pravilna")
+    }
+    
   }
  
   // addFav = (city) => {
@@ -62,10 +77,12 @@ class App extends React.Component {
   addFav = (city) => {
  
       let cityName = this.state.city;
+      console.log(cityName)
       if(citiesMap.indexOf(cityName) === -1) {
         citiesMap.push(this.state.city);
         localStorage.setItem('citiesMap', JSON.stringify(citiesMap));
-        this.setState({citiesMap: this.state.citiesMap});
+        citiesMap = JSON.parse(localStorage.getItem('citiesMap')) || [];
+        this.setState({citiesMap: citiesMap});
       } else {
         return
       }
@@ -95,6 +112,7 @@ class App extends React.Component {
   }
 onCityClick = async (e) => {
         const cityName = e;
+        cityName.toLowerCase();
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
         
@@ -113,19 +131,20 @@ onCityClick = async (e) => {
     return (
       <div>
         <div className="wrapper">
-          <div className="main">
+          
             <div className="container">
-              <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-6 col-xs-4 title-container">
+              <div className="row main">
+                <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 title-container">
                   <div className="favorites">
                     <h3>Favorites</h3> 
                     {
                       citiesMap.map(function(cityName, index) {
+                        
                         var classSelection = '';
-                        if(cityName.toLowerCase() === this.state.cityName.toLowerCase()) {
-                          classSelection = ' selected';
-                        }
-                      
+                        // if(cityName.toLowerCase() === this.state.cityName.toLowerCase()) {
+                        //   classSelection = ' selected';
+                        // }
+                        
                           return(
                            
                             <div className={"city-box" + classSelection} onClick={this.onCityClick.bind(this, cityName)}>
@@ -139,7 +158,7 @@ onCityClick = async (e) => {
                   
                   </div>
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-6 col-xs-8 form-container">
+                <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 form-container">
                   <Form getWeather={this.getWeather.bind(this)}/>
                   <Weather 
                     temperature={this.state.temperature} 
@@ -158,10 +177,13 @@ onCityClick = async (e) => {
               </div>
             </div>
           </div>
-        </div>
+        
       </div>
     );
   }
 };
+console.log(localStorage)
+console.log(citiesMap)
 citiesMap = JSON.parse(localStorage.getItem('citiesMap')) || [];
+// localStorage.clear();  
 export default App;
